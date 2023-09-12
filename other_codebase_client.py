f158d567e32b7dff996b2fa11579a6ea8578221a
RonaldGalea <raulronaldgalea@gmail.com>
2023-09-12 18:18:17 +0200

    thread inspecting task

--- other_codebase_client.py
+++ other_codebase_client.py
@@ -12,4 +12,14 @@ def my_dummy_task(arg1: int, arg2: str) -> str:
     return result.get()
 
 
+def inspect_pid_and_thread():
+    # Define a signature for the task - this must be the same as what the worker expects
+    task_signature = signature("tasks.inspect_pid_and_thread")
+
+    # Send the task using the signature
+    result = task_signature.delay()
+    return result.get()
+
+
 print(my_dummy_task(22, "string arg"))
+print(inspect_pid_and_thread())
